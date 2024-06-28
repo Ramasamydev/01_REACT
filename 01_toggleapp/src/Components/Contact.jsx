ad38e76764476a4cd3058ce1efcbdf73e3815b01
Ramasamydev <ramdevtech2021@gmail.com>
2024-06-28 17:40:41 +0530

    Add files via upload

---
+++ 01_toggleapp/src/Components/Contact.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Contact() {
+  return (
+    <div>
+        <h1>Contact</h1>
+        <p>
+           is libero reiciendis voluptatibus nobis quas, ex deserunt soluta ipsam voluptatum eaque cumque iste? Asperiores, consectetur.
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus nostrum expedita fugiat doloremque? Officia deserunt excepturi nulla neque ducimus facilis sit, quibusdam aut laborum odio dolorum, magni conseqet consectetur adipisicing elit. Ratione modi nostrum deserunt. Non, deleniti eveniet. Quo laudantium veritatis natus sit nostrum culpa eos possimus? Perferendis est porro adipisci deleniti recusandae?
+           met consectetur adipisicing elit. Tempora natus accusamus in eius exercitationem nostrum, assumenda quo fugit, et, est ex rerum doloremque dolorum illo dolorem architecto. Eos, rerum quam.
+           Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laboriosam tempora cumque dicta reprehenderit repudiandae nulla excepturi enim veritatis voluptatem debitis sunt dolorem unde, ex minima ducimus soluta quam iusto optio.
+        </p>
+    </div>
+  )
+}
+
+export default Contact
---
+++ 01_toggleapp/src/Components/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+        <h1>Home</h1>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio exercitationem, placeat qui, hic fugit perspiciatis libero reiciendis voluptatibus nobis quas, ex deserunt soluta ipsam voluptatum eaque cumque iste? Asperiores, consectetur.
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus nostrum expedita fugiat doloremque? Officia deserunt excepturi nulla neque ducimus facilis sit, quibusdam aut laborum odio dolorum, magni consequuntur esse ut!
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione modi nostrum deserunt. Non, deleniti eveniet. Quo laudantium veritatis natus sit nostrum culpa eos possimus? Perferendis est porro adipisci deleniti recusandae?
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempora natus accusamus in eius exercitationem nostrum, assumenda quo fugit, et, est ex rerum doloremque dolorum illo dolorem architecto. Eos, rerum quam.
+        </p>
+    </div>
+  )
+}
+
+export default Home

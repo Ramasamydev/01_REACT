9ecfeaf814a38e1695e88f510e211aae24ba04e7
Ramasamydev <ramdevtech2021@gmail.com>
2024-06-28 19:38:26 +0530

    Add files via upload

---
+++ 001_reacttutorial/04_reactapi/src/App.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from 'react'
+import Userone from './Components/Userone';
+import Usertwo from './Components/Usertwo';
+import Datagenerator from './Components/Datagenerator';
+import Allist from './Components/Allist';
+
+function App() {
+  // const [show,setShow] = useState(false);
+  // const showHideComponents = ()=>{
+  //     setShow(!show)
+  // }
+  return (
+    <div>
+        {/* <button onClick={showHideComponents}>CLICK</button>
+       {
+          show === true ? <Userone/> : <Usertwo/>
+       } */}
+
+       <Datagenerator />
+       {/* <Allist /> */}
+    </div>
+  )
+}
+
+export default App
---
+++ 001_reacttutorial/05_routerapi/src/App.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+import Home from './Pages/Home'
+import Sampledata from './Pages/Sampledata'
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <ul class="nav">
+          <li class="nav-item">
+            <Link to="/" className='nav-link'>HOME</Link>         
+
+          </li>
+          <li class="nav-item">
+          <Link to="/sample" className='nav-link'>SAMPLE DATA</Link>
+          </li>
+
+        </ul>
+        <Routes>
+          <Route path='/' exact element={<Home />}></Route>
+          <Route path='/sample' exact element={<Sampledata />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
